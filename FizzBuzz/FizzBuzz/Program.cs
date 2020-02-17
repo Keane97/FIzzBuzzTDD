47e7d90b88ca5421fbf695e183be9a9dd1bfc56d
Liam Keane <34273900+Keane97@users.noreply.github.com>
2020-02-17 14:53:21 +0000

    Add files via upload

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzzer fb = new FizzBuzzer();
+            for (int i = 0; i < 101; i++)
+            {
+                Console.WriteLine(fb.FizzBuzz(i));
+            }
+            Console.ReadKey();
+        }
+    }
+
+    public class FizzBuzzer
+    {
+        public string FizzBuzz(int input)
+        {
+            string result = string.Empty;
+            if (input % 3 == 0)
+            {
+                result = "Fizz";                //If Devisable by 3, result is equal to "Fizz"
+            }
+            if (input % 5 == 0)
+            {
+                result += "Buzz";               //If Devisable by `5, result is equal to "Buzz"
+            }
+            if (result == string.Empty)
+            {
+                result = input.ToString();      //Stringify the result if the value is empty
+            }
+            //return "1";  
+            return result;                      //returns result
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzzTests/FizzBuzzTests.cs
@@ -0,0 +1,25 @@
+﻿using FizzBuzz;
+using NUnit.Framework;
+using System;
+
+namespace FizzBuzzTests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [TestCase(1, "1")]      //passing TestCase (FizzBuzz return "1")
+        [TestCase(2, "2")]      //Failing TestCase (FizzBuzz return "1"), Passing TestCase (FizzBuzz return "2")
+        [TestCase(3, "Fizz")]   //Test for Fizz (input/3)
+        [TestCase(5, "Buzz")]   //Test for buzz (input/5)
+        [TestCase(6, "Fizz")]   //Test for Fizzbuzz (input/3)
+        [TestCase(10, "Buzz")]   //Test for Fizzbuzz (input/5)
+        [TestCase(15, "FizzBuzz")]   //Test for Fizzbuzz (input /5 && /3)
+        public void FizzBuzzer_GivenInt_returnsString(int input, string expected)   
+        {
+           
+            FizzBuzzer fb = new FizzBuzzer();       //instantiates a variable of class FizzBuzzer
+            string actual = fb.FizzBuzz(input);     //declares local var actual, calls the Fizzbuzz method and assigns the input value to the local var 'actual'
+            Assert.AreEqual(expected, actual);       // Checks if the input matches the expected value
+        } 
+    }
+}
